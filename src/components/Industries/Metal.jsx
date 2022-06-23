e02997b39874970b0c26778e45b7869d3adb340c
kartikpatel-dev <kv123dholariya@gmail.com>
2022-06-23 17:01:10 +0530

    Industries menu all pages change

--- src/components/Industries/Metal.jsx
+++ src/components/Industries/Metal.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './industry.css'
 import { API_BASE_URL, BASE_URL, API_IMG_URL } from '../../config'
-import { ReseachPaperSurveyItem } from './ReseachPaperSurveyItem'
 import { Link } from 'react-router-dom'
 import { OtherIndustryItem } from './OtherIndustryItem'
 import { GetInvolved } from '../ServiceInsights/GetInvolved'
 import { ServicesAskQuote } from '../ServicesAskQuote/ServicesAskQuote';
 import AOS from "aos";
+
+function descLimit(text, size) {
+    return text?.length > size ? text.substr(0, size - 1) + '...' : text;
+}
 
 function Metal() {
 
@@ -59,24 +62,28 @@ function Metal() {
                                 <div className='row'>
                                     <div className='col-lg-8 offset-lg-4'>
                                         <div className='industry-banner-ebook'>
-                                            <div className='industry-banner-form'>Form</div>
-                                            <div className='industry-banner-dwn'>
-                                                <div className='industry-banner-dwn-title'>E book download</div>
-                                            </div>
+                                            <img src={`${BASE_URL}/img/industries/CORPORALITY-STRIKERS-bg.png`} alt={`CORPORALITY STRIKERS`} className="industry-cs" />
+                                            <img src={`${BASE_URL}/img/industries/cs-pattern.png`} alt={`cs-pattern`} className="industry-cs-pattern" />
+                                            <Link to={`/`} className='industry-cs-link'>Download</Link>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className='row industry-banner-paper-survey-row'>
-                                    <ReseachPaperSurveyItem
-                                        title={`Reseach Paper`}
-                                        slug={``}
-                                    />
+                                    <div className='col-md-6'>
+                                        <div className='industry-banner-paper-survey-item industry-banner-paper'>
+                                            <div className='paper-survey-item-title'>Reseach Paper</div>
+                                            <div className='paper-survey-item-subtitle'>CAN THE MEDTECH INDUSTRY LOOK UP TO MARKETING FOR A SOLUTION?</div>
+                                            <Link to={`/`} className='paper-survey-item-link'>Start Reading</Link>
+                                        </div>
+                                    </div>
 
-                                    <ReseachPaperSurveyItem
-                                        title={`Survey`}
-                                        slug={``}
-                                    />
+                                    <div className='col-md-6'>
+                                        <div className='industry-banner-paper-survey-item industry-banner-survey'>
+                                            <img src={`${BASE_URL}/img/industries/your-om.png`} alt={`your-om`} className="industry-your-om" />
+                                            <Link to={`/`} className='paper-survey-item-link'>Get Started</Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -98,15 +105,18 @@ function Metal() {
                         <div className='col-lg-6'>
                             <div className='industry-blog-name-desc'>
                                 {
-                                    latestIndustrialArticle ?
-                                        <p>{latestIndustrialArticle.short_description}</p>
+                                    latestIndustrialArticle.title ?
+                                        <p>
+                                            <span dangerouslySetInnerHTML={{ __html: descLimit(latestIndustrialArticle.short_description ?? latestIndustrialArticle.description, 250) }}></span>
+                                            <Link to={`/industry/${latestIndustrialArticle.slug}`}>Read More</Link>
+                                        </p>
                                         : null
                                 }
 
                                 {
                                     realtedIndustrialArticles && realtedIndustrialArticles.length > 0 ?
                                         <>
-                                            <p>Read the related blog</p>
+                                            <p className='industry-blog-name-related'>Read the related blog</p>
                                             <ul>
                                                 {
                                                     realtedIndustrialArticles.map((item) =>
@@ -120,16 +130,6 @@ function Metal() {
                                         : null
                                 }
                             </div>
-
-                            {
-                                realtedIndustrialArticles && realtedIndustrialArticles.length > 0 ?
-                                    <div className="row">
-                                        <div className='col-lg-12'>
-                                            <Link to={`/industry/category/${category}`} className='read_more_cat_ia'>Read More Blogs <svg viewBox="0 0 10 16" focusable="false"><path d="m1.83288 15c-.20616 0-.42731-.0737-.58971-.2361-.324226-.3242-.324226-.8402 0-1.1645l5.58722-5.60223-5.58722-5.58722c-.324226-.32422-.324226-.84022 0-1.16445.32422-.324795.84022-.324795 1.16444-.01497l6.17694 6.17694c.1624.1624.23612.36857.23612.58971s-.08869.4273-.23612.58971l-6.17694 6.17691c-.1624.1624-.36857.2361-.57473.2361z" fill="currentColor" stroke="currentColor" strokeWidth=".831256"></path></svg><svg viewBox="0 0 10 16" focusable="false"><path d="m1.83288 15c-.20616 0-.42731-.0737-.58971-.2361-.324226-.3242-.324226-.8402 0-1.1645l5.58722-5.60223-5.58722-5.58722c-.324226-.32422-.324226-.84022 0-1.16445.32422-.324795.84022-.324795 1.16444-.01497l6.17694 6.17694c.1624.1624.23612.36857.23612.58971s-.08869.4273-.23612.58971l-6.17694 6.17691c-.1624.1624-.36857.2361-.57473.2361z" fill="currentColor" stroke="currentColor" strokeWidth=".831256"></path></svg></Link>
-                                        </div>
-                                    </div>
-                                    : null
-                            }
                         </div>
 
                         <div className='col-lg-6'>
